e5fa9666c47c20adef026091313aa8163deecee1
XiaoBaiCai2 <69073923+XiaoBaiCai2@users.noreply.github.com>
2020-08-16 01:07:32 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -38,6 +38,35 @@ function  xiajiang(obj,num,css) {
     },num*1000);
 }
 
+setTimeout(function(){
+	xiajiang($(".ly-box01"),15,'top1');
+    xiajiang($(".ly-stxt01"),15,'text1');
+    xiajiang($(".ly-stxt02"),15,'text2');
+    xiajiang($(".ly-stxt03"),15,'text1');
+    xiajiang($(".ly-stxt04"),15,'text2');
+    xiajiang($(".ly-stxt05"),15,'text1');
+    xiajiang($(".ly-stxt06"),15,'text2');
+    xiajiang($(".ly-stxt07"),15,'text1');
+    xiajiang($(".ly-stxt08"),15,'text2');
+    xiajiang($(".ly-stxt09"),15,'text1');
+},1000);
+
+setTimeout(function(){
+	xiajiang($(".ly-box02"),10,'top2');
+    xiajiang($(".ly-box02>.img1"),10,'img1');
+    xiajiang($(".ly-box02>.img2"),10,'img1');
+	},16000);
+setTimeout(function(){
+	xiajiang($(".ly-box03"),10,'top3');
+	xiajiang($(".ly-box03>.img1"),10,'img1');
+	xiajiang($(".ly-box03>.img2"),10,'img1');
+	 },26000);
+setTimeout(function(){
+	xiajiang($(".ly-box04"),10,'top4');
+	xiajiang($(".ly-box04>.img1"),10,'img1');
+	xiajiang($(".ly-box04>.img2"),10,'img1');
+},36000);
+  
 
 
 
